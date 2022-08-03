a0df3e49fa2657805f8abd1d49c65142a246ba52
siaris <wirawan.is@gmail.com>
2022-08-03 17:34:59 +0700

    views

--- application/views/kronologi/new.php
+++ application/views/kronologi/new.php
@@ -5,7 +5,7 @@
 </section>
 <section class="content-header">
     <div class="row">
-        <form action="<?= BASEURL?>/kronologi/set/" method="post" enctype="multipart/form-data" accept-charset="utf-8">
+        <form action="<?= BASEURL?>/kronologi/set_status/" method="post" enctype="multipart/form-data" accept-charset="utf-8">
         <div class="col-md-12">
           <!-- general form elements -->
           <div class="box box-primary">
@@ -16,9 +16,12 @@
             <!-- form start -->
             <form role="form">
               <div class="box-body">
+              <input type="hidden" name="platform" value="">
+              <input type="hidden" name="pmi_news" value="<?= $idPmiNews?>">
+              <input type="hidden" name="last_status" value="<?= $last_status?>">
+              <input type="hidden" name="loc" value="<?= $my_province?>">    
                 <div class="form-group">
                   <label for="exampleInputEmail1">Status</label>
-                  <input type="hidden" value="<?= $idPmiNews?>">
                   <select name="status" class="form-control" >
                     <?foreach($next as $n){?>
                         <option value="<?= $n?>"><?= $allS[$n]?></option>
