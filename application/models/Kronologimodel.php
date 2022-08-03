30863f86beacc936145f03826c1f132dac93fda2
siaris <wirawan.is@gmail.com>
2022-08-03 17:34:26 +0700

    model

--- application/models/Kronologimodel.php
+++ application/models/Kronologimodel.php
@@ -3,10 +3,10 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class NewsLabormodel extends MY_Model {
+class Kronologimodel extends MY_Model {
     function __construct() {
-        $this->table = 'news_labor';
+        $this->table = 'kronologi_perjalanan';
+        $this->primary_key = 'id';
         $this->load_table($this->table);
-        $this->primary_key = 'uuid';
     }
 }
