701e2dda6ee18021f9e3bce8d3e368999d73aba4
siaris <wirawan.is@gmail.com>
2022-10-11 03:36:20 +0700

    user list in config module

--- application/controllers/Config.php
+++ application/controllers/Config.php
@@ -27,11 +27,30 @@ class Config extends MY_Controller {
 
     public function list_user(){
         $this->prep_bootstrap();
+        $this->listProv = $this->config->item('provinsi');
+        $this->load->model('Usermodel');
 
         $this->load->library('grocery_crud/Grocery_CRUD_extended');
         $C = new Grocery_CRUD_extended();
         $C->set_table('user')
         ->columns('username','name','group','status')
+        ->fields('username','password','name','group','status','map_user_function','wilayah_operasi')
+        ->field_type('password','password')
+        ->callback_before_insert(array($this,'insert_collect_data_user'))
+        ->callback_before_update(array($this,'update_collect_data_user'))
+        ->set_relation('group','group','name')
+        ->field_type('status','dropdown',['non active','active'])
+        ->field_type('map_user_function','invisible')
+        ->callback_add_field('wilayah_operasi',function($v,$r){
+            array_unshift($this->listProv,'');
+            return $this->val_select('wilayah_operasi','',$this->listProv);})
+        ->callback_edit_field('wilayah_operasi',function($v,$r){
+            $this->get_j = $this->get_j($r);
+            array_unshift($this->listProv,'');
+            return $this->val_select('wilayah_operasi',$this->get_j['wilayah_operasi'],$this->listProv);})
+        ->callback_edit_field('password',function($v,$r){
+            return '<input value="'.$v.'" name="password" type="password" readonly>';})
+        ->required_fields('username','password','name','group','status')
         ->unset_delete()
         ->unset_read();
         $D = $C->render(); 
@@ -39,6 +58,27 @@ class Config extends MY_Controller {
         $this->template->write_view("content", 'grocery_crud_content',$D);
         
         $this->template->render();
+    }
+
+    function get_j($id){
+        $j = $this->Usermodel->queryOne('id = '.$id,'map_user_function','');
+        return !empty($j)?json_decode($j,TRUE):[];
+    }
+
+    public function val_select($name,$val,$ref){
+        return gc_val_select($name,$val,$ref);
+    }
+
+    function insert_collect_data_user($p){
+        $p['password'] = md5($p['password']); 
+        return $this->update_collect_data_user($p);
+    }
+
+    function update_collect_data_user($p){
+        $p['map_user_function'] = json_encode(['wilayah_operasi'=>$p['wilayah_operasi']]);
+        unset($p['wilayah_operasi']);  
+        //insert to user group
+        return $p;
     }
 
     public function groups(){
