ddacf1ae462d3f7c013555c844c8b6c08d628b45
siaris <wirawan.is@gmail.com>
2022-10-22 05:58:02 +0700

    api

--- application/controllers/Apis.php
+++ application/controllers/Apis.php
@@ -27,4 +27,32 @@ class Apis extends REST_Controller {
         $R = $m->find_all($m->table.'.id = '.$id);
         $this->response($R, REST_Controller::HTTP_OK);
     }
+
+    public function dashboard_post(){
+        $this->load->model('NewsLabormodel');
+        $q = !empty($this->input->post('q'))?$this->get_q_dashboard($this->input->post('q')):' and true ';
+        $R = $this->NewsLabormodel->query('select * from 
+        (select labor.json_v,news_labor.status,json,group_concat(kronologi_perjalanan.uuid) kronologi from news_labor
+        inner join labor on labor_id = labor.id
+        inner join news on news_id = news.id
+        left join kronologi_perjalanan on news_labor_id = news_labor.uuid
+        where true group by news_labor.uuid)
+        x where true '.$q)->result_array();
+        $this->response($R, REST_Controller::HTTP_OK); 
+    }
+
+    private function get_q_dashboard($v){
+        $vQ = explode(':',$v);
+        $return = '';
+        switch($vQ[0]){
+            case 'UPT':
+                $return = ' and kronologi like "%.31" ';
+                break;
+            default:
+                $key = $vQ[0] == 'TUJUAN'?'kecamatan_domisili':'jenis_kelamin';
+                $return = ' and json_v like \'"'.$key.'":"'.$vQ[1].'"\' ';
+                break;
+        }
+        return $return;
+    }
 }
