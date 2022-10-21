b6ef90f1190402cee0d8166551b799814f8ddfa0
siaris <wirawan.is@gmail.com>
2022-10-21 07:00:28 +0700

    dasboard : bentuk kumpulan filter pada controller

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -18,6 +18,14 @@ class Front extends MY_Controller {
     public function home(){
         $this->prep_bootstrap();
         $D['message'] = ['Coming Soon'];
+        $dd = [];
+        foreach($this->config->item('jenis_kelamin') as $k=>$v) $dd['jenis_kelamin:'.$k] = 'jenis kelamin: '.$v;
+        foreach($this->config->item('provinsi') as $k=>$v) $dd['UPT:'.$k] = 'UPT: '.$v;
+        $this->load->model('Wilayahmodel');
+        $R = $this->Wilayahmodel->find_all_tujuan(array_keys($this->config->item('provinsi')));
+        foreach($R as $v) $dd['TUJUAN:'.$v['id_wilayah']] = 'TUJUAN: '.$v['nama_wilayah'];
+
+        $D['dd'] = $dd;
         
         $this->template->write_view("content", 'home',$D);
         $this->template->render();
